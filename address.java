70a82ecf4dc7a9bc50a40b8e48eb19e2e6fb2f5c
arshima-pathan <aashimak031@gmail.com>
2026-07-03 07:43:11 +0530

    Add files via upload

---
+++ address.java
@@ -0,0 +1,11 @@
+
+public class address
+{
+     public static void main(String[] args)
+    {
+        System.out.println("lakheshwar society");
+        System.out.println("street no.9");
+        System.out.println("\"pedak road pachhal-rajkot\"");
+        System.out.println("pincode-360003");
+    }
+}
---
+++ resume.java
@@ -0,0 +1,42 @@
+
+public class resume
+{
+     public static void main(String[] args)
+    {
+        System.out.println("1. PERSONAL INFORMATION");
+        System.out.println("NAME: pathan arshima firojkhan");
+        System.out.println("CONTACT NO. : 70395.....");
+        System.out.println("EMAIL : aashimak031@gmail.com");
+        System.out.println("CITY STATE : rajkot-gujrat");
+        System.out.println("LINKDIN : Arshima pathan");
+        System.out.println("\n\n");
+        
+        System.out.println("2. CAREER OBJECTIVE");
+        System.out.println("Motivated Bsc.it student seeking opportunities to apply programming and problem-solving skills while gaining practical experience in the it industry");
+        System.out.println("\n\n");
+        
+        System.out.println("3. EDUCATION");
+        System.out.println("DEGREE NAME : Bsc.It");
+        System.out.println("UNIVERSITY : Atmiya University-Rajkot");
+        System.out.println("YOS : 2025-2028");
+        System.out.println("\n\n");
+        
+        System.out.println("4. TECHNICAL SKILLS");
+        System.out.println("PROGRAMMING LANG : C,Python");
+        System.out.println("WEB TECHNOLOGIES : HTML,CSS,Java script");
+        System.out.println("DATABASE : Mysql");
+        System.out.println("TOOLS : Ms Office,Vs Code");
+        System.out.println("\n\n");
+        
+        System.out.println("5. KNOWN LANGUAGE");
+        System.out.println("ENGLISH");
+        System.out.println("HINDI");
+        System.out.println("GUJRATI");
+        System.out.println("\n\n");
+        
+        System.out.println("6. HOBBIES & INTERESTS");
+        System.out.println("CODING");
+        System.out.println("LEARNING TECHNOLOGIES");
+        System.out.println("\n\n");
+    }
+}
