6b6e98b998fd19eb5b264f2817c12d64deffa2be
arshima-pathan <aashimak031@gmail.com>
2026-09-25 08:59:22 +0530

    Add files via upload

--- empdemo.java
+++ empdemo.java
@@ -1,16 +1,20 @@
-class emp
+class employee
 {
-    int id;
-    String name;
-    void getemp(int n,String nm)
+    int id,salary;
+    String name,city;
+    void getemp(int n,String nm,int s1, String ct)
     {
         id=n;
         name=nm;
+        salary=s1;
+        city=ct;
     }
   void putemp()
   {
       System.out.println("employee id="+id);
         System.out.println("employee name="+name);
+        System.out.println("salary="+salary);
+        System.out.println("city="+city);
   }
     
 }
@@ -18,10 +22,10 @@ public class empdemo
 {
   public static void main(String args[])
   {
-     emp e1=new emp(); 
-     emp e2=new emp(); 
-     e1.getemp(1,"arshima");
-     e2.getemp(2,"tanvi");
+     employee e1=new employee(); 
+     employee e2=new employee(); 
+     e1.getemp(1,"arshima",80000,"rajkot");
+     e2.getemp(2,"tanvi",80000,"rajkot");
      e1.putemp();
      System.out.println("--------");
      e2.putemp();
